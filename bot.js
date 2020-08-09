09cab50c1985dcf8fe69b14847a26c89012d5b27
Finx1 <49606811+Finx1@users.noreply.github.com>
2020-08-09 11:22:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("57961656945016834")
+let channel =     client.channels.get("740476713414426718")
 setInterval(function() {
 channel.send(`بنصير مليونرات ولكم مليون`);
 }, 30)
